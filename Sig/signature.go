abfcc9e8febc4ff65783dee92ac95eb391d61f9c
unbule <weixin1128211@163.com>
2021-10-06 16:42:42 +0800

    example

--- Sig/signature.go
+++ Sig/signature.go
@@ -1,5 +1,14 @@
 package Sig
 
+// import "github.com/xlcetc/cryptogm/sm/sm9"
+// then use func MasterKeyGen(rand io.Reader) (mk *MasterKey, err error) to get mk
+// such as
+// mk, err := sm9.MasterKeyGen(rand.Reader)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 		return
+// 	}
+
 import (
 	"log"
 
